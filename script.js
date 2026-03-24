63a4b96b249cfe6c6b0916574053582aab432d12
Marttbox <Martinmartinezfernandez09@gmail.com>
2026-03-23 20:05:14 -0600

    Enhance lightbox display with active class and timeout
    Refactor lightbox functionality to include active class and timeout for display.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    setTimeout(() => {
+        lightbox.classList.add("active");
+    }, 10);
+    imagenGrande.src = img.src;
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+    setTimeout(() => {
+        lightbox.style.display = "none";
+    }, 300);
+});
 
 const lightbox = document.getElementById("lightbox");
 const imagenGrande = document.getElementById("imagenGrande");
